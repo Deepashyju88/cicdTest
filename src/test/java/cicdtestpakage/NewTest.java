d226551347e5bd089fd0240f2bd9e757740c9bec
Deepa <deepaheera77@gmail.com>
2026-01-03 18:07:42 -0600

    cicd test

---
+++ src/test/java/cicdtestpakage/NewTest.java
@@ -0,0 +1,26 @@
+package cicdtestpakage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void fun() {
+	  
+	  System.out.println("test for safty *****************************************");
+  }
+  
+  @Test
+  public void googleTest() {
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.google.com/");
+	  
+	  driver.findElement(By.name("q")).sendKeys("javascript api");
+	  
+	  System.out.println("test for safty *****************************************");
+  }
+}
---
+++ src/test/java/cicdtestpakage/newT.java
@@ -0,0 +1,12 @@
+package cicdtestpakage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class newT {
+  @Test
+  public void fun2() {
+	  
+	  System.out.println("check cicd**************************************************");
+  }
+}
